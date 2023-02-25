9035b5b403c51458370171afefb7e2ddbfe8b12b
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-02-25 19:16:58 +0530

    Track data

--- src/Pages/TrainLivePage/TrainLIvePage.js
+++ src/Pages/TrainLivePage/TrainLIvePage.js
@@ -2,20 +2,28 @@ import React from 'react'
 import { traindata } from '../../Atom/Atom'
 import { useRecoilState } from 'recoil'
 import style from "./TrainLivePage.module.css"
+import Image from "./Track.png"
 
 function TrainLivePage() {
 const [data,setData] = useRecoilState(traindata)
 console.log(data,"bipullllll")
   return (
-    <div>
-      <h1>TrainLivePage</h1>
+    <div className={style.main}>
+      <img className={style.img} src={Image} />
+      <div className={style.box}>
       {data.map((item)=>
       <>
-      <div className={style.main}>
-         <h3>{item.source_name}</h3>
-      </div>
+      
+      <div className={style.TrainBox}>
+        
+        <h3>{item.source_name}</h3>
+        <h5>Arival time:{item.actual_arrival_time}</h5>
+        <h5>Departure time:{item.actual_departure_time}</h5>
+        </div>
+    
       </>
       )}
+      </div>
     </div>
   )
 }
