68e4664654e9ab8fcccaf0d5acdb8bb2803eec42
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-02-26 00:37:20 +0530

    train track

--- src/Pages/TrainLivePage/TrainLIvePage.js
+++ src/Pages/TrainLivePage/TrainLIvePage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect , useState } from 'react'
 import { traindata } from '../../Atom/Atom'
 import { useRecoilState } from 'recoil'
 import style from "./TrainLivePage.module.css"
@@ -7,17 +7,18 @@ import Image from "./Track.png"
 function TrainLivePage() {
 const [data,setData] = useRecoilState(traindata)
 console.log(data,"bipullllll")
+const [color,setcolor] = useState("blue")
 
 const date = new Date();
 const hour=date.getHours()
 const min = date.getMinutes()
 const sec= date.getSeconds()
-
-
 console.log(hour,min,sec)
 
 
-
+// useEffect(()=>{
+//   // data.map()
+// },[])
 
   return (
     <div className={style.main}>
@@ -28,7 +29,7 @@ console.log(hour,min,sec)
       
       <div className={style.TrainBox}>
       <img className={style.img} src={Image} />
-        <h3>{item.source_name}</h3>
+        <h3 style={{color : item.actual_departure_time.split("").slice(0,2).join("") == hour  ? "red" : "blue" } }>{item.source_name}</h3>
         <h5>Arival time:{item.actual_arrival_time}</h5>
         <h5>Departure time:{item.actual_departure_time}</h5>
         </div>
