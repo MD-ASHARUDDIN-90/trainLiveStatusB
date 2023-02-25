632a9969986c85bd290efee7913ab316c66e5676
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-02-25 21:49:59 +0530

    train

--- src/Pages/TrainLivePage/TrainLIvePage.js
+++ src/Pages/TrainLivePage/TrainLIvePage.js
@@ -9,13 +9,13 @@ const [data,setData] = useRecoilState(traindata)
 console.log(data,"bipullllll")
   return (
     <div className={style.main}>
-      <img className={style.img} src={Image} />
+      {/* <img className={style.img} src={Image} /> */}
       <div className={style.box}>
       {data.map((item)=>
       <>
       
       <div className={style.TrainBox}>
-        
+      <img className={style.img} src={Image} />
         <h3>{item.source_name}</h3>
         <h5>Arival time:{item.actual_arrival_time}</h5>
         <h5>Departure time:{item.actual_departure_time}</h5>
